16dd1a68ca2e84222b6f031fb79debba885452e9
Maor Sharon <maor@zappy5.com>
2025-12-02 22:22:44 +0200

    Update business information: Business details updated

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const phoneHeaderBtn = document.querySelector('.phone-header-btn');
   if (phoneHeaderBtn) {
     phoneHeaderBtn.addEventListener('click', function() {
-      const phoneNumber = '[business_phone]';
+      const phoneNumber = '050-2456418';
       window.location.href = 'tel:' + phoneNumber;
     });
   }
